459c856ded2283f864fc83a8dc06ae5d204dfc94
David Barrat <david@barrat.io>
2023-11-15 23:53:49 +0100

    chore: setup jest

---
+++ jest.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    moduleFileExtensions: [
+      'js',
+      'ts',
+      'tsx',
+    ],
+    transform: {
+      '^.+\\.(ts|tsx)?$': [
+        'ts-jest'
+      ],
+    },
+    modulePaths: [
+      "<rootDir>",
+    ],
+    moduleNameMapper: {
+      '^@authdog/(.*)$': '<rootDir>/packages/$1/src',
+    },
+    testMatch: [
+      '**/**/*.test.(ts|js)',
+      '**/test/**/*.test.(ts|js)',
+    ],
+    testPathIgnorePatterns: [
+      '/node_modules/',
+      'build',
+    ],
+    testEnvironment: 'node',
+    rootDir: '.',
+    collectCoverageFrom: [
+      '!<rootDir>/src',
+    ],
+    preset: 'ts-jest',
+  }
+  
---
+++ packages/core/jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    // setupFilesAfterEnv: ['./jest.setup.js'],
+    transform: {
+      '^.+\\.tsx?$': ['ts-jest', {//the content you'd placed at "global"
+        babel: true,
+        tsconfig: 'tsconfig.json',
+      }]
+    },
+    "transformIgnorePatterns": [
+      "/node_modules/",
+    ],
+    // moduleNameMapper: {
+    //   '^@authdog\/platform-utils$': require.resolve('@authdog/platform-utils'),
+    // }
+  }
+  
