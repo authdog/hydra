74216a75ed211b8d41aedddc4a5810c218ea4a7c
David Barrat <david@barrat.io>
2023-12-14 14:00:51 +0100

    implement raw schema generation file command

--- packages/cli/src/commands/generate-schema.ts
+++ packages/cli/src/commands/generate-schema.ts
@@ -1,16 +1,36 @@
-export const generateSchemaAction = ({
-    config
+import {buildSchemaIntrospection} from "../utils/introspectSchemas";
+
+export const generateSchemaAction = async ({
+    config = {
+        
+    }
 }) => {
 
-    let hydraConfig = {};
+    // let hydraConfig = {};
 
-    if (config) {
-        // validate custom config
-    } else {
-        // check if config exists
-        // validate standard config
-    }
-    // if valid, set hydraConfig as parsed config
+    // if (config) {
+    //     // validate custom config
+    // } else {
+    //     // check if config exists
+    //     // validate standard config
+    // }
+    // // if valid, set hydraConfig as parsed config
+
+    const schemas = [
+          {
+            id: "mgt",
+            uri: "https://mgt.auth.dog/graphql",
+          },
+          {
+            id: "authz",
+            uri: "https://authz.auth.dog/graphql",
+          },
+    ];
+    
+    const outputPath = "./.hydra/schemaRaw.ts";
+
+    await buildSchemaIntrospection(schemas, outputPath);
+
 
     console.log("Generate Schema action");
 }
---
+++ packages/cli/src/utils/introspectSchemas.ts
@@ -0,0 +1,89 @@
+import fetch from "node-fetch";
+import { getIntrospectionQuery, buildClientSchema, printSchema } from "graphql";
+
+async function introspectRemoteSchema(endpointUrl: string) {
+  try {
+    // Send an introspection query to the GraphQL endpoint
+    const response = await fetch(endpointUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ query: getIntrospectionQuery() }),
+    });
+
+    if (!response.ok) {
+      throw new Error(
+        `Failed to fetch introspection query: ${response.status}`,
+      );
+    }
+
+    const introspectionResult = await response.json();
+
+    // Build a client schema from the introspection result
+    const clientSchema = buildClientSchema(introspectionResult.data);
+    const schemaString = printSchema(clientSchema);
+
+    // Create a multi-line string enclosed within backticks
+    const formattedSchemaString = `${schemaString}`;
+
+    console.log(formattedSchemaString);
+
+    return formattedSchemaString;
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
+}
+
+
+interface GraphQLSchema {
+  id: string;
+  uri: string;
+  token?: string; // some schemas require a token to be introspected
+}
+
+
+
+export const buildSchemaIntrospection = async (schemas:GraphQLSchema[], outputPath: string) => {
+  let schemaWithIntrospection = [];
+
+  for (const schema of schemas) {
+    const introspected = await introspectRemoteSchema(schema.uri);
+
+    const output = {
+      name: schema.id,
+      url: schema.uri,
+      introspected,
+    };
+
+    schemaWithIntrospection.push(output);
+  }
+
+  // const outputPath = "src/handlers/federation/schemaRawGenerated.ts";
+  const exportStatements = schemaWithIntrospection.map(
+    (s) =>
+      `export const ${s.name} = {
+        name: "${s.name}",
+        url: "${s.url}",
+        introspected: ${JSON.stringify(s.introspected, null, 2)}
+      };`,
+  );
+
+  const fileContent = `${exportStatements.join("\n")}\n`;
+
+  const fs = require("fs");
+
+  try {
+
+    // define .hydra folder if it doesn't exist
+    if (!fs.existsSync(".hydra")) {
+      fs.mkdirSync(".hydra");
+    }
+
+    fs.writeFileSync(outputPath, fileContent);
+    console.log(`${outputPath} written`);
+  } catch (err) {
+    console.error(err);
+  }
+};
+// buildSchemaIntrospection();
