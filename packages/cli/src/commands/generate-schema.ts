42c7d1ca0fc53bc8a9c054ea6368d11fa6175cbc
David Barrat <david@barrat.io>
2023-12-17 04:03:02 +0100

    add dynamic transpiling from cli

--- packages/cli/src/commands/generate-schema.ts
+++ packages/cli/src/commands/generate-schema.ts
@@ -1,6 +1,8 @@
-import path from "path";
 import { buildSchemaIntrospection } from "../utils/introspectSchemas";
 import { validateConfig } from "../utils/validateConfig";
+import path from "path";
+import ts from "typescript";
+import fs from "fs"
 
 interface IGenerateSchemaAction {
   config?: string;
@@ -14,29 +16,48 @@ export const generateSchemaAction = async ({
   const configPath = path.resolve(rootPath, config); // Construct absolute path for config
   const outputPath = path.resolve(rootPath, ".hydra/schemaRaw.ts"); // Construct absolute path for output
 
-  let demoConfig: any;
-
   try {
-    // Use `import` instead of `require` for TypeScript support
-    const importedConfig = await import(configPath);
-    demoConfig = importedConfig.default || importedConfig;
+
+    // Compiler options (optional)
+    const tsConfig = {
+      target: ts.ScriptTarget.ESNext,
+      module: ts.ModuleKind.CommonJS,
+    };
+
+    // Create TypeScript compiler host and program
+    const compilerHost = ts.createCompilerHost({});
+    const program = ts.createProgram([configPath], tsConfig, compilerHost);
+
+    // Get the source file
+    const sourceFile = program.getSourceFile(configPath);
+
+    if (!sourceFile) {
+      throw new Error("Source file not found");
+    }
+
+    // Emit the transpiled code
+    const { outputText } = ts.transpileModule(sourceFile.getText(), { compilerOptions: tsConfig });
+
+    // // Evaluate the transpiled code
+    const module_ = eval(outputText);
+
+    const demoConfig = module_.default || module_;
+
+    const validatedConfig = validateConfig(demoConfig);
+
+    if (!validatedConfig) {
+      throw new Error("Invalid config");
+    }
+
+    try {
+      await buildSchemaIntrospection(validatedConfig.schemas, outputPath);
+    } catch (error) {
+      console.error(error);
+      throw new Error("Error generating schema");
+    }
+
+    console.info("Schema generated successfully");
   } catch (error) {
-    throw new Error("Error loading or parsing the config file: " + error);
+    throw new Error("Error transpiling or executing TypeScript code: " + error);
   }
-
-  console.log(demoConfig);
-  const validatedConfig = validateConfig(demoConfig);
-
-  if (!validatedConfig) {
-    throw new Error("Invalid config");
-  }
-
-  try {
-    await buildSchemaIntrospection(validatedConfig.schemas, outputPath);
-  } catch (error) {
-    console.error(error);
-    throw new Error("Error generating schema");
-  }
-
-  console.info("Schema generated successfully");
 };
