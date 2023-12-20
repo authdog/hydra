3cca64807beec1bd7a229a0f3718a99c0c70a909
David Barrat <david@barrat.io>
2023-12-20 02:16:57 +0100

    cleanup seo

--- apps/docs/theme.config.tsx
+++ apps/docs/theme.config.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 import { GiHydra } from "react-icons/gi";
 import { useRouter } from "next/router";
-import { useConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
   logo: (
@@ -32,23 +31,13 @@ const config: DocsThemeConfig = {
     ),
   },
   faviconGlyph: "🐉",
-  head: () => {
-    const { asPath, defaultLocale, locale } = useRouter();
-    const { frontMatter } = useConfig();
-    const url =
-      "https://my-app.com" +
-      (defaultLocale === locale ? asPath : `/${locale}${asPath}`);
-
-    return (
-      <>
-        <meta property="og:url" content={url} />
-        <meta property="og:title" content={frontMatter.title} />
-        <meta
-          property="og:description"
-          content={frontMatter.description || "Hydra Documentation"}
-        />
-      </>
-    );
+  useNextSeoProps() {
+    const { asPath } = useRouter();
+    if (asPath !== "/") {
+      return {
+        titleTemplate: "%s – SWR",
+      };
+    }
   },
 };
 
