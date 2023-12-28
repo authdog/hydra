2c8c67d09c8df1ba563c178a70cbf1fd7f121059
David Barrat <david@barrat.io>
2023-12-28 20:50:37 +0100

    cleanup fetch rate with facet function, update lockfile

--- packages/core/src/do/utils.ts
+++ packages/core/src/do/utils.ts
@@ -1,17 +1,13 @@
-export const fetchWithRateLimiter = async (
-  req: Request,
-  rateLimiter,
-  facetId: string,
-) => {
-  const timestamp = new Date().toISOString().slice(0, 16).replace(/[-:T]/g, "");
+export const fetchRateLimiterWithFacet = async (req, rateLimiter, facetId, timestamp) => {
   let id = rateLimiter.idFromName(`HydraRateLimiter_${timestamp}`);
-  let stub = rateLimiter.get(id);
-
-  const url = new URL(req.url);
-  url.pathname = "/increment";
-  url.searchParams.set("facet", facetId);
-
-  const modifiedRequest = new Request(url.toString(), req);
-  const response = await stub.fetch(modifiedRequest);
-  return await response.json();
-};
+      let stub = rateLimiter.get(id);
+      const url = new URL(req.url);
+      url.pathname = "/increment";
+      url.searchParams.set("facet", facetId);
+    
+      const modifiedRequest = new Request(url.toString(), req?.clone());
+      const response = await stub.fetch(modifiedRequest);
+      const rateCountBody = await response.json();
+      const {value: rateCount} = rateCountBody;
+      return rateCount;
+}
--- packages/core/src/handlers/hydra.ts
+++ packages/core/src/handlers/hydra.ts
@@ -8,22 +8,7 @@ import { addTypenameKeywordToSchema, removeTypename } from "../schemaUtils";
 import { readStream } from "../utils/stream";
 import { GraphQLHandler } from "./graphql";
 import { checkTokenValidness } from "keylab";
-import { fetchWithRateLimiter } from "../do/utils";
-
-const fetchRateLimiterWithFacet = async (req, rateLimiter, facetId, timestamp) => {
-  let id = rateLimiter.idFromName(`HydraRateLimiter_${timestamp}`);
-      let stub = rateLimiter.get(id);
-      const url = new URL(req.url);
-      url.pathname = "/increment";
-      url.searchParams.set("facet", facetId);
-    
-      const modifiedRequest = new Request(url.toString(), req?.clone());
-      const response = await stub.fetch(modifiedRequest);
-      const rateCountBody = await response.json();
-      const {value: rateCount} = rateCountBody;
-      return rateCount;
-}
-
+import { fetchRateLimiterWithFacet } from "../do/utils";
 
 export const HydraHandler = async (req, env, ctx): Promise<Response> => {
   if (ctx.hasOwnProperty("kv") === false) {
