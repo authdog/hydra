20d7d07e93f02b893439847ff4feb4e2f6ff16a6
David Barrat <david@barrat.io>
2023-12-30 01:20:00 +0100

    cleanup

--- packages/cli/src/utils/cliLogger.ts
+++ packages/cli/src/utils/cliLogger.ts
@@ -1,4 +1,3 @@
-// @ts-ignore
 import cc from "node-console-colors";
 
 export const logError = (message: string) => {
--- packages/core/src/do/HydraRateLimiter.test.ts
+++ packages/core/src/do/HydraRateLimiter.test.ts
@@ -1,14 +1,14 @@
-import { HydraRateLimiter } from './HydraRateLimiter'; // Update the import path as needed
+import { HydraRateLimiter } from "./HydraRateLimiter"; // Update the import path as needed
 
-describe('HydraRateLimiter', () => {
+describe("HydraRateLimiter", () => {
   let rateLimiter: HydraRateLimiter;
 
   beforeEach(() => {
     rateLimiter = new HydraRateLimiter();
   });
 
-  it('should increment facet value correctly', () => {
-    const facet = 'test_facet';
+  it("should increment facet value correctly", () => {
+    const facet = "test_facet";
 
     rateLimiter.incrementFacetValue(facet);
     rateLimiter.incrementFacetValue(facet);
@@ -17,21 +17,21 @@ describe('HydraRateLimiter', () => {
     expect(value).toBe(2);
   });
 
-  it('should return default value for non-existing facet', () => {
-    const facet = 'non_existing_facet';
+  it("should return default value for non-existing facet", () => {
+    const facet = "non_existing_facet";
 
     const value = rateLimiter.getFacetValue(facet);
     expect(value).toBe(0);
   });
 
-  it('should handle fetching based on endpoint path', async () => {
+  it("should handle fetching based on endpoint path", async () => {
     const incrementRequest = {
-      url: 'http://localhost:3566/increment?facet=test_facet',
+      url: "http://localhost:3566/increment?facet=test_facet",
       /* Add other necessary request properties as needed */
     };
 
     const defaultRequest = {
-      url: 'http://localhost:3566/default',
+      url: "http://localhost:3566/default",
       /* Add other necessary request properties as needed */
     };
 
@@ -41,5 +41,4 @@ describe('HydraRateLimiter', () => {
     const defaultValue = await rateLimiter.fetch(defaultRequest);
     expect(defaultValue.status).toBe(404);
   });
-
 });
